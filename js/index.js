4acebfccebe32747e1c541f218206ef3706bbaec
Talent Antonio <antonio.talenty@gmail.com>
2019-02-05 18:19:45 +0000

    Task 1 and task 2 done, working on task 3

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
 // Your code goes here
+const eventList = [
+  
+    'mouseover',
+    'mousemove',
+    'drag',
+    'drop',
+    'keydown',
+    'keyup',
+    'load',
+    'resize',
+    'scroll',
+    'focus',
+    'dblclick',
+  ];
+  
+  // getting a callback function ready
+  function eventLogger(eventObject) {
+    const { type, target, currentTarget, shiftKey } = eventObject;
+
+    shiftKey
+        ? document.body.style.backgroundColor = 'blue'
+        : document.body.style.backgroundColor = 'Lavender';
+    
+  
+    console.log(`-> ${type} event <-
+     currentTarget: ${currentTarget && currentTarget.nodeName} 💥
+        (event target: ${target && target.nodeName})
+      `);
+  }
+  
+  const allElements = Array.from(document.getElementsByTagName('*'));
+  
+  allElements.forEach((element) => {
+    eventList.forEach((event) => {
+      element.addEventListener(event, eventLogger);
+    });
+  });
+
+  
+  
